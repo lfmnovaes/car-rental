8de3fa05ea7dc92202c1ae6b156ee5f49a21d35e
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-02-17 12:05:37 -0300

    Add foreign keys to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_17_145429) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_17_150439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_17_145429) do
     t.date "date_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "car_id", null: false
+    t.bigint "city_id", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["city_id"], name: "index_reservations_on_city_id"
   end
 
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "cities"
 end
