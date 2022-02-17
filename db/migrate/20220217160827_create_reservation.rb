a5b629a84245d44c4e05822c37f16963fee80427
enmanuel <enmanuellassis@gmail.com>
2022-02-17 12:15:37 -0400

    Add tables to db

---
+++ db/migrate/20220217160827_create_reservation.rb
@@ -0,0 +1,10 @@
+class CreateReservation < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.date :date_start
+      t.date :date_end
+
+      t.timestamps
+    end
+  end
+end
