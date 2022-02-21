0e2e257a1f3d3b90db98533cc0d34d0b95199948
Luis Fernando <lfmnovaes@gmail.com>
2022-02-21 10:25:07 -0300

    Add cars output examples, and add rack-cors

--- Gemfile
+++ Gemfile
@@ -71,4 +71,5 @@ group :test do
   gem 'webdrivers'
 end
 
+gem 'rack-cors'
 gem 'rubocop', '>= 1.0', '< 2.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Car.create(brand: "Toyota", model: "Corolla", year: "2019", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg/1920px-2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg", reserved: true, price: 40)
+Car.create(brand: "Audi", model: "A8", year: "2018", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/2018_Audi_A8_50_TDi_Quattro_Automatic_3.0.jpg/1920px-2018_Audi_A8_50_TDi_Quattro_Automatic_3.0.jpg", reserved: false, price: 60)
